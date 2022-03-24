5a216940f8210ba928bc4a67251a1dfb80af7f38
Kirill-Kondratev <kk@x-lab.com.ru>
2022-03-25 04:50:19 +0700

    Проверка на существование папка assets или её создание

--- bin/models/generate-folder.ts
+++ bin/models/generate-folder.ts
@@ -1,8 +1,11 @@
 import fs from "fs";
-import ansi, {bgMagenta} from "ansi-colors";
+import {bgMagenta} from "ansi-colors";
 import {logger} from "../helpers";
 
 export const generateFolder = (dirName: string) => {
+  if (!fs.existsSync('./bin/assets')) {
+    fs.mkdirSync('./bin/assets');
+  }
   const path = `./bin/assets/${dirName}`;
   try {
     fs.mkdirSync(path)
